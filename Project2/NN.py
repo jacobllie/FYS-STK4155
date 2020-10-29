4112bc798d7353ce9f8c92854580a6f750085425
meudnaes <eliasudnes@hotmail.com>
2020-10-29 16:12:15 +0100

    Neural ntework worikng! :)

--- Project2/NN.py
+++ Project2/NN.py
@@ -7,10 +7,9 @@ random.seed(100)
 
 class dense_layer:
     def __init__(self, inputs, outputs, act_func):
-        self.weights = 1e-3*random.randn(inputs, outputs)
+        self.weights = random.randn(inputs, outputs)
         self.b = random.randn(1, outputs)
         self.act_func = act_func
-        #self.a, self.da, self.z = None
 
     def __call__(self, X):
         #calculate activation
@@ -24,8 +23,7 @@ class NN:
     def __init__(self, layers):
         self.layers = layers
 
-    def __call__(self, a):
-        #feed forward
+    def feed_forward(self, a):
         for layer in self.layers:
             a = layer(a)
         return a
@@ -33,39 +31,37 @@ class NN:
     def backprop(self, cost, x, y, eta):
         L = self.layers
         a = L[-1].a
-        delta_l = (L[-1].weights@((a - y)*L[-1].act_func.deriv(L[-1].z)).T).T
+        delta_l = cost.deriv(y, a)*L[-1].da
         for i in reversed(range(1, len(L)-1)):
-            delta_l = delta_l*L[i].act_func.deriv(L[i].z)
-            L[i].weights = L[i].weights - (eta*delta_l.T@L[i-1].a).T
+            delta_l = (delta_l@L[i+1].weights.T)*L[i].da
+            L[i].weights = L[i].weights - eta*(L[i-1].a.T@delta_l)
             L[i].b = L[i].b - eta*delta_l
-            delta_l = delta_l@L[i].weights.T
         #First hidden layer
-        delta_l = delta_l*L[0].act_func.deriv(L[0].z)
-        L[0].weights = L[0].weights - (eta*delta_l.T@x).T
+        delta_l = (delta_l@L[1].weights.T)*L[0].da
+        L[0].weights = L[0].weights - eta*(x.T@delta_l)
         L[0].b = L[0].b - eta*delta_l
-        delta_l = delta_l@L[0].weights.T
+        self.feed_forward(x)
 
 class MSE:
     def __call__(self, y, a):
-        return 1/len(y)*np.sum(y - a)**2
+        return 1/len(y)*np.sum(a - y)**2
 
     def deriv(self, y, a):
         return 2/len(y)*(a - y)
 
-#define cost function
-
-x = np.linspace(0,1,100).reshape(-1,1) + 0.01*random.randn(100, 1)
+x = np.linspace(0,1,100).reshape(-1,1) + 0.1*random.randn(100,1)
 y = x**2
-layer1 = dense_layer(1, 50, sigmoid())
-layer2 = dense_layer(50, 20, sigmoid())
-layer3 = dense_layer(20, 1, identity())
+layer1 = dense_layer(1, 10, sigmoid())
+layer2 = dense_layer(10, 10, sigmoid())
+layer3 = dense_layer(10, 20, sigmoid())
+layer4 = dense_layer(20, 5, sigmoid())
+layer5 = dense_layer(5, 1, identity())
 
-layers = [layer1, layer2, layer3]
+layers = [layer1, layer2, layer3, layer4, layer5]
 network = NN(layers)
-network(x)
+network.feed_forward(x)
 mse = MSE()
-print(mse(y, layer3.a))
+print(mse(y, layers[-1].a))
 for i in range(1000):
-    network.backprop(MSE, x, y, 1)
-    network(x)
-print(mse(y, layer3.a))
+    network.backprop(MSE(), x, y, 0.5)
+print(mse(y, layers[-1].a))
