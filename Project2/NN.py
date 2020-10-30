09f528e5ec430e515678070eeb8ce3aacfe86e15
meudnaes <eliasudnes@hotmail.com>
2020-10-30 10:35:31 +0100

    NN

--- Project2/NN.py
+++ Project2/NN.py
@@ -1,7 +1,10 @@
 from numpy import random
-from activation_function import sigmoid, identity
+from activation_function import sigmoid, identity, relu, leaky_relu
 import numpy as np
 from numpy import random
+import matplotlib.pyplot as plt
+from data_prep import data_prep
+from functions import FrankeFunction
 
 random.seed(100)
 
@@ -26,20 +29,23 @@ class NN:
     def feed_forward(self, a):
         for layer in self.layers:
             a = layer(a)
-        return a
 
     def backprop(self, cost, x, y, eta):
+        #Starting with output layer
         L = self.layers
         a = L[-1].a
         delta_l = cost.deriv(y, a)*L[-1].da
+        #Looping over layer from output to 1st hidden
         for i in reversed(range(1, len(L)-1)):
             delta_l = (delta_l@L[i+1].weights.T)*L[i].da
+            #Updating weights
             L[i].weights = L[i].weights - eta*(L[i-1].a.T@delta_l)
-            L[i].b = L[i].b - eta*delta_l
-        #First hidden layer
+            #Updating biases
+            L[i].b = L[i].b - eta*delta_l[0,:]
+        #First hidden layer, here a = x
         delta_l = (delta_l@L[1].weights.T)*L[0].da
         L[0].weights = L[0].weights - eta*(x.T@delta_l)
-        L[0].b = L[0].b - eta*delta_l
+        L[0].b = L[0].b - eta*delta_l[0,:]
         self.feed_forward(x)
 
 class MSE:
@@ -48,20 +54,54 @@ class MSE:
 
     def deriv(self, y, a):
         return 2/len(y)*(a - y)
-
-x = np.linspace(0,1,100).reshape(-1,1) + 0.1*random.randn(100,1)
-y = x**2
+#Test case
+"""x = np.linspace(0,1,100).reshape(-1,1)
+y = x**2 + 0.1*random.randn(100,1)
 layer1 = dense_layer(1, 10, sigmoid())
 layer2 = dense_layer(10, 10, sigmoid())
-layer3 = dense_layer(10, 20, sigmoid())
-layer4 = dense_layer(20, 5, sigmoid())
-layer5 = dense_layer(5, 1, identity())
+layer3 = dense_layer(10, 1, identity())
+#layer4 = dense_layer(20, 5, sigmoid())
+#layer5 = dense_layer(5, 1, identity())
 
-layers = [layer1, layer2, layer3, layer4, layer5]
+layers = [layer1, layer2, layer3]
 network = NN(layers)
 network.feed_forward(x)
 mse = MSE()
 print(mse(y, layers[-1].a))
-for i in range(1000):
-    network.backprop(MSE(), x, y, 0.5)
+for i in range(100):
+    network.backprop(mse, x, y, 0.5)
 print(mse(y, layers[-1].a))
+plt.plot(x, y, label='Training data')
+plt.plot(x, layers[-1].a, label='Neural network')
+plt.legend()
+plt.show()"""
+
+n = 100
+noise = 0.1
+x = np.random.uniform(0,1,n)
+y = np.random.uniform(0,1,n)
+x,y = np.meshgrid(x,y)
+z = np.ravel(FrankeFunction(x,y) + noise*np.random.randn(n,n))
+
+data = data_prep(x,y,z,1)
+train_input, test_input, train_output, test_output = data()    #X_train,X_test,z_train,z_test
+train_output = np.reshape(train_output,(-1,1))               #the shape was (x,) we needed (x,1) for obvious reasons
+test_output = np.reshape(test_output,(-1,1))
+x_train = train_input[:, [1,2]]
+x_test = test_input[:, [1,2]]
+
+layer1 = dense_layer(2, 10, relu())
+layer2 = dense_layer(10, 6, relu())
+layer3 = dense_layer(6, 1, identity())
+
+layers = [layer1, layer2, layer3]
+network = NN(layers)
+network.feed_forward(x_train)  #sending in x and y from design matrix.
+mse = MSE()
+for i in range(1000):
+    network.backprop(MSE(), x_train,train_output, 0.1)
+
+#Test the network on the test data
+print(mse(train_output, layers[-1].a))
+network.feed_forward(x_test)
+print(mse(test_output, layers[-1].a))
