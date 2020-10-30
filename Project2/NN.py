03d1de67588fbd6183683e4bf5ec6745108ba25f
jacobllie <jacob.l.lie97@gmail.com>
2020-10-30 11:15:46 +0100

    I have made code to run keras/tensorflow. The code is able to run several learning rates if you want to.

--- Project2/NN.py
+++ Project2/NN.py
@@ -46,20 +46,16 @@ class NN:
             delta_l = (delta_l@L[i+1].weights.T)*L[i].da
             #Updating weights
             L[i].weights = L[i].weights - eta*(L[i-1].a.T@delta_l)
-<<<<<<< HEAD
+
             #Updating biases
             L[i].b = L[i].b - eta*delta_l[0,:]
         #First hidden layer, here a = x
         delta_l = (delta_l@L[1].weights.T)*L[0].da
         L[0].weights = L[0].weights - eta*(x.T@delta_l)
         L[0].b = L[0].b - eta*delta_l[0,:]
-=======
-            L[i].b = (L[i].b - eta*delta_l)[0,:]    #picking out the first row since we only have one bias per neuron
-        #First hidden layer
-        delta_l = (delta_l@L[1].weights.T)*L[0].da
-        L[0].weights = L[0].weights - eta*(x.T@delta_l)
-        L[0].b = (L[0].b - eta*delta_l)[0,:]
->>>>>>> 0d69a5f6b21fcfa83d608bb1eb27d49ebfe2dc72
+
+
+
         self.feed_forward(x)
 
 class MSE:
@@ -68,145 +64,95 @@ class MSE:
 
     def deriv(self, y, a):
         return 2/len(y)*(a - y)
-<<<<<<< HEAD
+
 #Test case
-"""x = np.linspace(0,1,100).reshape(-1,1)
-y = x**2 + 0.1*random.randn(100,1)
-layer1 = dense_layer(1, 10, sigmoid())
-layer2 = dense_layer(10, 10, sigmoid())
-layer3 = dense_layer(10, 1, identity())
-#layer4 = dense_layer(20, 5, sigmoid())
-#layer5 = dense_layer(5, 1, identity())
-=======
+if __name__ == "__main__":
 
-#x = np.linspace(0,1,100).reshape(-1,1) + 0.1*random.randn(100,1)
-#y = x**2
+    n = 100
+    noise = 0.1
+    x = np.random.uniform(0,1,n)
+    y = np.random.uniform(0,1,n)
+    x,y = np.meshgrid(x,y)
+    z = np.ravel(FrankeFunction(x,y) + noise*np.random.randn(n,n))
 
-n = 100
-noise = 0.01
-x = np.random.uniform(0,1,n)
-y = np.random.uniform(0,1,n)
-x,y = np.meshgrid(x,y)
-z = np.ravel(FrankeFunction(x,y)+noise*np.random.randn(n,n))
+    data = data_prep(x,y,z,1)
+    train_input, test_input, train_output, test_output = data()    #X_train,X_test,z_train,z_test
+    train_output = np.reshape(train_output,(-1,1))               #the shape was (x,) we needed (x,1) for obvious reasons
+    test_output = np.reshape(test_output,(-1,1))
+    x_train = train_input[:, [1,2]]
+    x_test = test_input[:, [1,2]]
 
-data = data_prep(x,y,z,1)
-train_input,test_input,train_output,test_output = data()    #X_train,X_test,z_train,z_test
-train_output = np.reshape(train_output,(-1,1))               #the shape was (x,) we needed (x,1) for obvious reasons
-test_output = np.reshape(test_output,(-1,1))
+    layer1 = dense_layer(2, 10, sigmoid())
+    layer2 = dense_layer(10, 6, sigmoid())
+    layer3 = dense_layer(6, 1, identity())
 
-#print(test_output.shape)
+    layers = [layer1, layer2, layer3]
+    network = NN(layers)
+    network.feed_forward(x_train)  #sending in x and y from design matrix.
+    mse = MSE()
+    for i in range(1000):
+        network.backprop(MSE(), x_train,train_output, 0.1)
 
-layer1 = dense_layer(2, 10, sigmoid())
-layer2 = dense_layer(10, 5, sigmoid())
-layer3 = dense_layer(5, 1, identity())
+    #Test the network on the test data
+    print(mse(train_output, layers[-1].a))
+    network.feed_forward(x_test)
+    print(mse(test_output, layers[-1].a))
 
->>>>>>> 0d69a5f6b21fcfa83d608bb1eb27d49ebfe2dc72
-
-layers = [layer1, layer2, layer3]
-network = NN(layers)
-network.feed_forward(train_input[:,1:3])  #sending in x and y from design matrix.
-mse = MSE()
-<<<<<<< HEAD
-print(mse(y, layers[-1].a))
-for i in range(100):
-    network.backprop(mse, x, y, 0.5)
-print(mse(y, layers[-1].a))
-plt.plot(x, y, label='Training data')
-plt.plot(x, layers[-1].a, label='Neural network')
-plt.legend()
-plt.show()"""
-
-n = 100
-noise = 0.1
-x = np.random.uniform(0,1,n)
-y = np.random.uniform(0,1,n)
-x,y = np.meshgrid(x,y)
-z = np.ravel(FrankeFunction(x,y) + noise*np.random.randn(n,n))
-
-data = data_prep(x,y,z,1)
-train_input, test_input, train_output, test_output = data()    #X_train,X_test,z_train,z_test
-train_output = np.reshape(train_output,(-1,1))               #the shape was (x,) we needed (x,1) for obvious reasons
-test_output = np.reshape(test_output,(-1,1))
-x_train = train_input[:, [1,2]]
-x_test = test_input[:, [1,2]]
-
-layer1 = dense_layer(2, 10, relu())
-layer2 = dense_layer(10, 6, relu())
-layer3 = dense_layer(6, 1, identity())
-
-layers = [layer1, layer2, layer3]
-network = NN(layers)
-network.feed_forward(x_train)  #sending in x and y from design matrix.
-mse = MSE()
-for i in range(1000):
-    network.backprop(MSE(), x_train,train_output, 0.1)
-
-#Test the network on the test data
-print(mse(train_output, layers[-1].a))
-network.feed_forward(x_test)
-print(mse(test_output, layers[-1].a))
-=======
-#print(mse(train_output, layers[-1].a))
-for i in range(100):
-    network.backprop(MSE(), train_input[:,1:3],train_output, 0.5)
-#print(mse(train_output, layers[-1].a))
-
-network.feed_forward(test_input[:,1:3])
-#print(mse(test_output,layers[-1].a))
+    network.feed_forward(x_test)
+    #print(mse(test_output,layers[-1].a))
 
 
-#comparing with tensorflow and sklearn
+    #comparing with tensorflow and sklearn
 
-from keras.models import Sequential
-from keras.layers import Dense
-from keras.regularizers import l2
-from keras.optimizers import SGD
-from keras.models import Sequential
-from keras.layers import Dense
-from keras.regularizers import l2
-from keras.optimizers import SGD
+    from keras.models import Sequential
+    from keras.layers import Dense
+    from keras.regularizers import l2
+    from keras.optimizers import SGD
+    from keras.models import Sequential
+    from keras.layers import Dense
+    from keras.regularizers import l2
+    from keras.optimizers import SGD
 
-epochs = 100
-batch_size = n
-n_neurons_layer1 = 10
-n_neurons_layer2 = 5
-n_categories = 1
-eta_vals = np.array([0])
-lmbd_vals = np.array([0])
+    epochs = 100
+    batch_size = n
+    n_neurons_layer1 = 10
+    n_neurons_layer2 = 5
+    n_categories = 1
+    eta_vals = np.array([0.5])
+    lmbd_vals = np.array([0])
 
 
-def create_neural_network_keras(n_neurons_layer1, n_neurons_layer2, n_categories, eta, lmbd):
-    model = Sequential()
-    model.add(Dense(n_neurons_layer1, activation='sigmoid'))
-    model.add(Dense(n_neurons_layer2, activation='sigmoid'))
-    model.add(Dense(n_categories))
+    def create_neural_network_keras(n_neurons_layer1, n_neurons_layer2, n_categories, eta, lmbd):
+        model = Sequential()
+        model.add(Dense(n_neurons_layer1, activation='sigmoid'))
+        model.add(Dense(n_neurons_layer2, activation='sigmoid'))
+        model.add(Dense(n_categories))
 
-    sgd = SGD(lr=eta)
-    model.compile(loss='mean_squared_error', optimizer=sgd, metrics=['mse'])
+        sgd = SGD(lr=eta)
+        model.compile(loss='mean_squared_error', optimizer=sgd, metrics=['mse'])
 
-    return model
+        return model
 
-DNN_keras = np.zeros((len(eta_vals), len(lmbd_vals)), dtype=object)
+    DNN_keras = np.zeros((len(eta_vals), len(lmbd_vals)), dtype=object)
 
 
 
 
 
-create_neural_network_keras(n_neurons_layer1,n_neurons_layer2,n_categories,eta,lmbd)
 
 
 
 
-for i, eta in enumerate(eta_vals):
-    for j, lmbd in enumerate(lmbd_vals):
-        DNN = create_neural_network_keras(n_neurons_layer1, n_neurons_layer2, n_categories,eta=eta, lmbd=lmbd)
-        DNN.fit(X_train, Y_train, epochs=epochs, batch_size=batch_size, verbose=0)
-        scores = DNN.evaluate(X_test, Y_test)
 
-        DNN_keras[i][j] = DNN
+    for i, eta in enumerate(eta_vals):
+        for j, lmbd in enumerate(lmbd_vals):
+            DNN = create_neural_network_keras(n_neurons_layer1, n_neurons_layer2, n_categories,eta=eta, lmbd=lmbd)
+            DNN.fit(x_train, train_output, epochs=epochs, batch_size=batch_size, verbose=0)
+            scores = DNN.evaluate(x_train, train_output)
 
-        print("Learning rate = ", eta)
-        print("Lambda = ", lmbd)
-        print("Test accuracy: %.3f" % scores[1])
-        print()
->>>>>>> 0d69a5f6b21fcfa83d608bb1eb27d49ebfe2dc72
+            DNN_keras[i][j] = DNN
+
+            print("Learning rate = ", eta)
+            print("Lambda = ", lmbd)
+            print("Test mse: %.3f" % scores[1])
+            print()
