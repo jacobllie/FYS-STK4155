35c5e91d7b3e924363647ef1a7d97c542cdc71bc
Jonas <Jonastf@student.matnat.uio.no>
2020-11-04 11:34:45 +0100

    CE in cost_functions.py

--- Project2/cost_functions.py
+++ Project2/cost_functions.py
@@ -11,5 +11,18 @@ class accuracy:
     def __call__(self,y,a):
         I = [1 for i in range(len(y)) if y[i]==a[i]]
         return np.sum(I)/len(y)
+        
     def deriv(self):
         return 0.01
+
+class CE:
+    def __call__(self,y,a):
+        m = len(y)
+        return -(1/m) * np.sum(y-np.log(a) + (1-y)*np.log(1-a))
+
+    def deriv(self,y,a):
+        m = len(y)
+        derivs = []
+        for i in range(m):
+            derivs.append((1/m) * np.sum((a - y) * a[i], axis=0))
+        return np.array(derivs)
