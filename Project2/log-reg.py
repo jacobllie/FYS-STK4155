5bb0d411b44cca0c867870b89bdce842149186d3
jacobllie <jacob.l.lie97@gmail.com>
2020-11-11 17:19:45 +0100

    timing the loop, and initialize layers for every learning rate and penalty

--- Project2/log-reg.py
+++ Project2/log-reg.py
@@ -11,6 +11,7 @@ from sklearn.preprocessing import StandardScaler
 from sklearn.metrics import confusion_matrix
 import seaborn as sb
 from sklearn.linear_model import SGDClassifier
+import time
 # download MNIST dataset
 
 data = datasets.load_digits()
@@ -40,19 +41,18 @@ scaler.fit(X_train)
 X_train = scaler.transform(X_train)
 X_test = scaler.transform(X_test)
 
-eta = np.logspace(-4,-1,4)
-penalty = np.logspace(-5,0,4)
+penalty = [0.001, 0.01, 0.05, 0.1, 0.5, 1]
+eta = [0.001, 0.005, 0.01, 0.05, 0.1, 0.5]
 #set up one-hot vector
 one_hot = np.zeros((Y_train.shape[0], 10))
 for i in range(Y_train.shape[0]):
     one_hot[i,Y_train[i]] = 1
 
 #sklearn's logistic regression
-sklearn_accuracy_best = 1    #arbitrary
+sklearn_accuracy_best = 0.5   #arbitrary
 sklearn_accuracy = np.zeros((len(eta),len(penalty)))
-print(X_train.shape)
-print(X_test.shape)
 
+start = time.time()
 for i in range(len(eta)):
     for j in range(len(penalty)):
         sklearn_logistic = SGDClassifier(loss="log", alpha = penalty[j],
@@ -60,43 +60,51 @@ for i in range(len(eta)):
         sklearn_logistic.fit(X_train,np.ravel(Y_train))
         sklearn_logistic.predict(X_test)
         sklearn_accuracy[i][j] = sklearn_logistic.score(X_test,np.ravel(Y_test))
-        if sklearn_accuracy[i][j] > sklearn_accuracy_best:
-            sklearn_accuracy_best = accuracy
-            print(sklearn_accuracy_best)
+print("keras time {:.3}s".format(time.time()-start))
 
-heatmap = sb.heatmap(sklearn_accuracy,cmap="viridis",
-                              xticklabels=["%.3f" %i for i in eta],
-                              yticklabels=["%.3f" %j for j in penalty],
+
+
+
+heatmap = sb.heatmap(sklearn_accuracy.T,cmap="viridis",
+                              xticklabels=eta,
+                              yticklabels=penalty,
                               cbar_kws={'label': 'Accuracy'},
                               fmt = ".5",
                               annot = True)
 heatmap.set_xlabel(r"$\eta$")
 heatmap.set_ylabel(r"$\lambda$")
 heatmap.invert_yaxis()
-heatmap.set_title("Accuracy on handwritten digits")
+heatmap.set_title("Accuracy on MNIST with sklearn SGDClassifier")
 fig = heatmap.get_figure()
+plt.yticks(rotation=0)
+fig.savefig("sklearn_accuracy_logreg.pdf",bbox_inches='tight',
+                                  pad_inches=0.1,
+                                  dpi = 1200)
+
 plt.show()
 
 
 
 
 #Make neural network with zero hidden layer
-output_layer = DenseLayer(features, 10, softmax())
 
-layers = [output_layer]
-log_net = NN(layers)
+
 cost_func = CE()    #using cross-entropy as cost function
 
-epochs = 300
-mini_batch_size = N//200
+epochs = 200
+mini_batch_size = 100
 
 m = X_train.shape[0]
 
 ind = np.arange(0, X_train.shape[0])
 cost_array = np.zeros((len(eta),len(penalty)))
-cost_best = 2
+cost_best = 0.5
+start = time.time()
 for i in range(len(eta)):
     for j in range(len(penalty)):
+        output_layer = DenseLayer(features, 10, softmax())
+        layers = [output_layer]
+        log_net = NN(layers)
         for k in range(epochs):     #looping epochs
             random.shuffle(ind)
             X_train = X_train[ind]
@@ -105,11 +113,9 @@ for i in range(len(eta)):
                 log_net.backprop2layer(cost_func, X_train[l:l+mini_batch_size],
                     one_hot[l:l+mini_batch_size], eta[i],penalty[j])
             Y_pred = np.argmax(log_net.feedforward(X_test), axis=1)
-            #print(Y_pred)
-            #print(Y_test)
 
-            cost_array[i][j] = accuracy()(Y_test, Y_pred)*100
 
+            cost_array[i][j] = accuracy()(Y_test, Y_pred)
             if cost_array[i][j] > cost_best:
                 print(cost_array[i][j])
                 penalty_best = penalty[j]
@@ -117,20 +123,27 @@ for i in range(len(eta)):
                 cost_best = cost_array[i][j]
                 Y_pred_best = Y_pred
                 Y_test_best = Y_test
+print("FFNN time {:.3}s".format(time.time()-start))
+#np.save("accuracy_FFNN_logreg.npy",cost_array)
 
 #making accuracy heatmap to compare with FFNN
 
-heatmap = sb.heatmap(cost_array,cmap="viridis",
-                              xticklabels=["%.3f" %i for i in eta],
-                              yticklabels=["%.3f" %j for j in penalty],
+heatmap = sb.heatmap(cost_array.T,cmap="viridis",
+                              xticklabels=eta,
+                              yticklabels=penalty,
                               cbar_kws={'label': 'Accuracy'},
                               fmt = ".5",
                               annot = True)
 heatmap.set_xlabel(r"$\eta$")
 heatmap.set_ylabel(r"$\lambda$")
 heatmap.invert_yaxis()
-heatmap.set_title("Accuracy on handwritten digits")
+heatmap.set_title("Accuracy on MNIST FFNN logreg")
 fig = heatmap.get_figure()
+plt.yticks(rotation=0)
+fig.savefig("FFNN_accuracy_logreg.pdf",bbox_inches='tight',
+                                  pad_inches=0.1,
+                                  dpi = 1200)
+
 plt.show()
 
 
@@ -144,15 +157,18 @@ heatmap = sb.heatmap(confusion_matrix,cmap="viridis",
                               xticklabels=["%d" %i for i in numbers],
                               yticklabels=["%d" %i for i in numbers],
                               cbar_kws={'label': 'Accuracy'},
-                              fmt = ".5",
-                              edgecolor="none")
+                              fmt = ".2",
+                              edgecolor="none",
+                              annot = True)
 heatmap.set_xlabel("pred")
 heatmap.set_ylabel("true")
 
 heatmap.set_title(r"MNIST prediction accuracy with $\lambda$ = {:.1e} $\eta$ = {:.1e}"\
         .format(penalty_best,eta_best))
 fig = heatmap.get_figure()
+plt.yticks(rotation=0)
 fig.savefig("MNIST_confusion.pdf",bbox_inches='tight',
                                   pad_inches=0.1,
                                   dpi = 1200)
+
 plt.show()
