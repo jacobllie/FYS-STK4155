a4f785f8c054b88ee889dad55f339d13b8fb24f5
jacobllie <jacob.l.lie97@gmail.com>
2020-12-09 10:35:25 +0100

    You can run for different learning rates and penalties, and the necessary values are stored in npy files that can be plotted in CNN_plot

--- Project3/src/CNN.py
+++ Project3/src/CNN.py
@@ -3,13 +3,17 @@ import matplotlib.pyplot as plt
 from data_adjustment import extract_data
 from tensorflow.keras.models import Sequential
 from tensorflow.keras.layers import Input, Dense, Conv2D, MaxPooling2D, Flatten
-from tensorflow.keras import optimizers, regularizers
+from tensorflow.keras import optimizers, regularizers,initializers
 from tensorflow.keras.utils import to_categorical
 from sklearn.model_selection import train_test_split
 from sklearn.preprocessing import StandardScaler
+from sklearn.metrics import confusion_matrix
+import seaborn as sb
+from numpy import save
 from PIL import Image
 import os
-
+from sklearn.preprocessing import StandardScaler
+import time
 
 class CNN_keras:
     def __init__(self, input_shape, receptive_field, n_filters,
@@ -38,9 +42,13 @@ class CNN_keras:
         First convolutional layer must contain the input shape,
         the other layers is not dependent of it
         """
-        self.model.add(Conv2D(self.nfilt, (self.recf,self.recf),
-                              input_shape=self.inp))
+        self.model.add(Conv2D(20, (self.recf,self.recf),
+            input_shape=self.inp,activation='relu', padding = 'same'))
         self.model.add(MaxPooling2D(pool_size=(2,2)))
+
+        self.model.add(Conv2D(20, (self.recf,self.recf),
+            input_shape=self.inp,activation='relu', padding = 'same'))
+        self.model.add(MaxPooling2D(pool_size=(2, 2)))
         #self.model.add(Conv2D(2*self.nfilt, (self.recf,self.recf)))
         #self.model.add(MaxPooling2D(pool_size=(2,2)))
 
@@ -64,20 +72,15 @@ class CNN_keras:
         if show_model==True: self.model.summary()
 
 
-
-
-
-
-
 if __name__ == '__main__':
 
-    paths = ["/data_images/Apple",
-             "/data_images/Banana",
-             "/data_images/Kiwi",
-             "/data_images/Mango",
-             "/data_images/Orange",
-             "/data_images/Pear",
-             "/data_images/Tomato"]
+    paths = ["../images/Apple/Total Number of Apples",
+             "../images/Banana",
+             "../images/Kiwi/Total Number of Kiwi fruit",
+             "../images/Mango",
+             "../images/Orange",
+             "../images/Pear",
+             "../images/Tomato"]
     true_labels = ["apple", "banana", "kiwi", "mango",
                    "orange", "pear", "tomato"]
 
@@ -104,7 +107,7 @@ if __name__ == '__main__':
 
 
 
-    im_shape = 50
+    im_shape = 100
     #data_size = data.data[0].shape
     data_size = (im_shape, im_shape, 3)
 
@@ -112,28 +115,16 @@ if __name__ == '__main__':
     filters = 20
     neuros_con = 50
 
-    eta = 0.0001
-    lmbd = 0.001
-    epochs = 1
-    batch_size = 10
-
-
-    CNN = CNN_keras(input_shape=data_size,
-                    receptive_field=rec_field,
-                    n_filters = filters,
-                    n_neurons_connected = neuros_con,
-                    labels = true_labels,
-                    eta = eta,
-                    lmbd = lmbd)
-
-    CNN.add_layer(show_model=False)
-
+    eta = np.array([0.0046]) #np.logspace(-4,-1,4)
+    lmbd = np.array([0.0001])#np.logspace(-4,-1,4)
+    epochs = 10
+    batch_size = 5
 
     """
     if im_shape is large (e.g. 200), max_data should be low (e.g. 500)
     if im_shape is low (e.g. 50), max_data can be large (e.g. 5000)
     """
-    max_data = 5000
+    max_data = 3000
     lim_data = int(max_data/len(paths))
     #tot_data = 0
     lens = []
@@ -141,8 +132,8 @@ if __name__ == '__main__':
         #tot_data += len(os.listdir("./"+path))
         lens.append(len(os.listdir("./"+path)))
     min_len = np.min(lens)
-    tot_data = min_len * len(paths)
-    runs = int(tot_data / max_data)
+    tot_data = (min_len * len(paths))
+    runs = int(tot_data / max_data)    #we will use the last run to test the final data
 
     print("---------------------------------------------")
     print("Unique fruits:           ", len(paths))
@@ -153,69 +144,81 @@ if __name__ == '__main__':
     print("Total fruit per run:     ", lim_data)
     print("---------------------------------------------")
     print()
+    pred = []
+    pred_labels = []
+    num_label = []
+    score_train = np.zeros((runs-1,2))
+    score_val = np.zeros((runs-1,2))
+    scaler = StandardScaler()
+    accuracy_map = np.zeros((len(eta),len(lmbd)))
+    best_score = 0
+    frac_data = np.zeros(runs-1)
+    accuracy_epoch = np.zeros((runs-1,epochs))
+    trainig_time = np.zeros(runs-1)
+    for j in range(len(eta)):
+        for k in range(len(lmbd)):
+            CNN = CNN_keras(input_shape=data_size,
+                            receptive_field=rec_field,
+                            n_filters = filters,
+                            n_neurons_connected = neuros_con,
+                            labels = true_labels,
+                            eta = eta[j],
+                            lmbd = lmbd[k])
 
-    #runs = 1
-
-    for i in range(runs):
-        print("Run:   ", i+1)
-        data = extract_data(paths, true_labels, lim_data=lim_data, from_data=i*lim_data)
-        data.reshape(im_shape)            # making all data the same shape
-        data.shuffle()
-        #data.gray()
-
-        #del data.data
-        X_train, X_test, y_train, y_test = train_test_split(data.data,
-                                                            data.hot_vector,
-                                                            train_size=0.8)
-
-        X_train, X_test = X_train/255, X_test/255               # scaling image data
+            CNN.add_layer(show_model=False)
+            for i in range(runs-1):
+                frac_data[i] = (i+1)*lim_data/max_data     #80% of data sent is used for training
+                print("Run:   ", i+1)
+                data = extract_data(paths, true_labels,
+                    lim_data=lim_data, from_data=i*lim_data)
+                data.reshape(im_shape)
 
 
-
-        CNN.model.fit(X_train, y_train, epochs=epochs, batch_size=batch_size, verbose=1)
-        scores = CNN.model.evaluate(X_test, y_test, verbose=1)
-
-        data.delete_all_data()          # clear the memory
-        print()
-
-
-
+                #del data.data
+                X_train, X_val, y_train, y_val = train_test_split(data.data,
+                                                                 data.hot_vector,
+                                                                 train_size=0.8)
+                print("{} images used for validation".format(X_val.shape[0]))
+                X_train,X_val = X_train/255,X_val/255  #Scaling each pixel value
+                #with max RGB value
 
 
+                start_time = time.time()
+                history = CNN.model.fit(X_train, y_train, epochs=epochs,
+                             batch_size=batch_size, verbose=1)
+                trainig_time[i] = time.time()-start_time
+                accuracy_epoch[i] = history.history["accuracy"]
+                print(accuracy_epoch[i])
+                score_train[i] = CNN.model.evaluate(X_train,y_train)
+                score_val[i] = CNN.model.evaluate(X_val, y_val, verbose=1)
+                data.delete_all_data()          # clear the memory
+                print("Accuracy on validation data with eta = {:.4}, lambda\
+                      = {:.4} is {:.4}".format(eta[j],lmbd[k],score_val[i,1]))
+            test_data = extract_data(paths,true_labels,lim_data=lim_data,
+                                    from_data=runs*lim_data)
+            test_data.reshape(im_shape)
+            scaled_test = test_data.data/255
+            scores = CNN.model.evaluate(scaled_test,test_data.hot_vector)
+            pred = CNN.model.predict(scaled_test)
+            accuracy_map[j][k] = score_val[i,1]
+            print("Accuracy on holy test data for lambda = {:.4},\
+                 eta = {:.4} is {:.4}".format(lmbd[k],eta[j],accuracy_map[j][k]))
+    #making confusion matrix
+            if score_val[-1,1] > best_score:
+                pred_num_label = np.argmax(pred,axis=1)
+                true_num_label = np.argmax(test_data.hot_vector,axis=1)
+                conf_matrix = confusion_matrix(true_num_label,pred_num_label,normalize="true")
+                print(conf_matrix)
+                best_score = scores[1]
+                print(best_score)
+    #storing the values for plotting
+                np.save("../data/CNN_result_pred",pred_num_label)
+                np.save("../data/CNN_result_true",true_num_label)
 
-    # predicting on a image that have not been used in training or testing
-
+                np.save("../data/CNN_confusion_matrix",conf_matrix)
     #test_im = np.array(Image.open("./test_images/test_apple.png"))
-    test = extract_data(["/test_images"], ["tomato"])
-    test.reshape(im_shape)
-    #test.gray()
-
-    pred = CNN.model.predict_step(test.data[:1,...])
-    plt.figure(figsize=[12,6])
-    plt.subplot(121)
-    plt.imshow(test.data[-1,...], cmap="gray")
-    plt.title("CNN predicts: %s" % (true_labels[np.argmax(pred.numpy())]))
-    for i in range(len(true_labels)):
-        plt.plot([0],[0], label="%s: %.3f" % (true_labels[i], pred.numpy()[0,i]))
-    plt.legend()
-    plt.subplot(122)
-    plt.imshow(test.real_data[-1,...], cmap="gray")
-    plt.title("Real data")
-
-    plt.tight_layout()
-    plt.show()
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-#
+    np.save("../data/CNN_accuracy_map",accuracy_map)
+    np.save("../data/CNN_accuracy_validation",score_val)
+    np.save("../data/CNN_frac_data",frac_data)
+    np.save("../data/CNN_accuracy_epoch",accuracy_epoch)
+    print("Time spent on training {:.4}s".format(np.sum(trainig_time)))
---
+++ Project3/src/CNN_plot.py
@@ -0,0 +1,119 @@
+import numpy as np
+import matplotlib.pyplot as plt
+import seaborn as sb
+import sys
+
+
+path = "../data/"
+
+
+create_conf_matrix = input("Analyse confidence matrix [Y/n]: ")
+
+if create_conf_matrix == "Y" or create_conf_matrix == "y":
+    create_conf_matrix = True
+elif create_conf_matrix == "N" or create_conf_matrix == "n":
+    create_conf_matrix = False
+else:
+    print("Please input Y or n!")
+    sys.exit()
+if create_conf_matrix == True:
+    true_labels = ["apple", "banana", "kiwi", "mango",
+                   "orange", "pear", "tomato"]
+    conf_matrix = np.load(path+"CNN_confusion_matrix.npy")
+    heatmap = sb.heatmap(conf_matrix,cmap="viridis",
+                          xticklabels=["%s" %i for i in true_labels],
+                          yticklabels=["%s" %i for i in true_labels],
+                          cbar_kws={'label': 'Accuracy'},
+                          fmt = ".3",
+                          edgecolor="none",
+                          annot = True)
+    plt.yticks(rotation=0)
+    heatmap.set_xlabel("pred")
+    heatmap.set_ylabel("true")
+
+    heatmap.set_title("Confusion matrix for fruit recognition")
+    fig = heatmap.get_figure()
+    #fig.savefig("../figures/CNN_confusion.pdf", bbox_inches='tight',
+                                                #pad_inches=0.1)
+    plt.show()
+
+create_accuracy_matrix = input("Analyse accuracy map [Y/n]: ")
+
+if create_accuracy_matrix == "Y" or create_accuracy_matrix == "y":
+    create_accuracy_matrix = True
+elif create_accuracy_matrix == "N" or create_accuracy_matrix == "n":
+    create_accuracy_matrix = False
+else:
+    print("Please input Y or n!")
+    sys.exit()
+if create_accuracy_matrix == True:
+    eta = np.logspace(-4,-1.5,4)
+    lmbd = np.logspace(-4,-1.5,4)
+    runs = 5
+    accuracy_map = np.load(path+"CNN_accuracy_map.npy")
+
+    heatmap = sb.heatmap(accuracy_map.T,cmap="viridis",
+                          xticklabels=["%.4f" %i for i in eta],
+                          yticklabels=["%.4f" %i for i in lmbd],
+                          cbar_kws={'label': 'Accuracy'},
+                          fmt = ".4",
+                          edgecolor="none",
+                          annot = True)
+    plt.yticks(rotation=0)
+    heatmap.set_xlabel(r"$\eta$")
+    heatmap.set_ylabel(r"$\lambda$")
+
+    heatmap.set_title("Accuracy map for fruit recognition")
+    fig = heatmap.get_figure()
+    #fig.savefig("../figures/CNN_accuracy_map.pdf", bbox_inches='tight',
+                                                   #pad_inches=0.1)
+    plt.show()
+
+
+create_val_accuracy = input("Analyse validation accuracy as a function of data trained [Y/n]: ")
+
+if create_val_accuracy == "Y" or create_val_accuracy == "y":
+    create_val_accuracy = True
+elif create_val_accuracy == "N" or create_val_accuracy == "n":
+    create_val_accuracy= False
+else:
+    print("Please input Y or n!")
+    sys.exit()
+if create_val_accuracy == True:
+
+    accuracy_val = np.load(path+"CNN_accuracy_validation.npy")
+    frac_data = np.load(path+"CNN_frac_data.npy")
+
+    plt.style.use("seaborn")
+    plt.title("Accuracy as a function of runs")
+    plt.xlabel("% of data used for training")
+    plt.ylabel("Accuracy")
+    plt.plot(frac_data,accuracy_val[:,1],"purple",label="Validation")
+    plt.legend()
+    plt.savefig("../figures/CNN_val_acc_layers.pdf",bbox_inches="tight",
+                                                     pad_inches=0.1)
+    plt.show()
+
+create_epoch_accuracy = input("Analyse accuracy of data at different % of data trained [Y/n]: ")
+
+if create_epoch_accuracy == "Y" or create_epoch_accuracy == "y":
+    create_epoch_accuracy = True
+elif create_epoch_accuracy == "N" or create_epoch_accuracy == "n":
+    create_epoch_accuracy = False
+else:
+    print("Please input Y or n!")
+    sys.exit()
+if create_epoch_accuracy == True:
+    frac_data = np.load(path+"CNN_frac_data.npy")
+    accuracy_epoch = np.load(path+"CNN_accuracy_epoch.npy")
+    print(accuracy_epoch)
+    plt.style.use("seaborn")
+    plt.title("Accuracy of training data at different stages of training")
+    plt.xlabel("Epochs")
+    plt.ylabel("Accuracy")
+    for i in range(len(accuracy_epoch)):
+        plt.plot(np.arange(accuracy_epoch.shape[1]),accuracy_epoch[i,:],label="{:.3}%".format(frac_data[i]))
+    plt.legend()
+    plt.savefig("../figures/CNN_acc_training.pdf",bbox_inches="tight",
+                                                  pad_inches=0.1)
+    plt.show()
